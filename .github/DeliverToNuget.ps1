8d956ecd2568eb06c9d9dc181a3ccca5273add57
mazhelez <mazhelez@microsoft.com>
2022-11-22 00:48:11 +0100

    Exclude AL-Go files from the package

--- .github/DeliverToNuget.ps1
+++ .github/DeliverToNuget.ps1
@@ -2,11 +2,42 @@ Param(
     [Hashtable] $parameters
 )
 
+function GenerateNuspec
+(
+    [Parameter(Mandatory=$true)]
+    $PackageId,
+    [Parameter(Mandatory=$true)]
+    $Version
+)
+{
+    [xml] $template = '<?xml version="1.0" encoding="utf-8"?>
+    <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+        <metadata>
+            <id></id>
+            <version></version>
+            <title>Dynamics SMB Business Central — System Application</title>
+            <authors>Microsoft</authors>
+            <owners>Microsoft</owners>
+            <requireLicenseAcceptance>false</requireLicenseAcceptance>
+            <description>System Application for D365 Business Central</description>
+            <summary>System Application for D365 Business Central</summary>
+        </metadata>
+	    <files>
+            <file src="SourceCode\**" target="SourceCode" exclude="**\.AL-Go\**"/>
+	    </files>
+    </package>'
+
+    $template.package.metadata.id = $PackageId
+    $template.package.metadata.version = $Version
+
+    return $template
+}
+
 try {
     $nuGetAccount = $parameters.Context | ConvertFrom-Json | ConvertTo-HashTable
     $nuGetServerUrl = $nuGetAccount.ServerUrl
     $nuGetToken = $nuGetAccount.Token
-    Write-Host "NuGetContext OK"
+    Write-Host "NuGetContext OK" -ForegroundColor Magenta
 }
 catch {
     throw "NuGetContext secret is malformed. Needs to be formatted as Json, containing serverUrl and token as a minimum."
@@ -18,68 +49,30 @@ $appsFolder = $parameters.appsFolder
 $testAppsFolder = $parameters.testAppsFolder
 $type = $parameters.type
 
-$packageParameters = @{
-    "gitHubRepository" = "$ENV:GITHUB_SERVER_URL/$ENV:GITHUB_REPOSITORY"
-}
+$packageId = "$($env:GITHUB_REPOSITORY_OWNER)-$($env:RepoName)"
 
-# Determine package ID
-if ($nuGetAccount.ContainsKey('PackageName')) {
-    $packageParameters.packageId = $nuGetAccount.PackageName.replace('{project}', $projectName).replace('{owner}', $ENV:GITHUB_REPOSITORY_OWNER).replace('{repo}',$env:RepoName)
-}
-else {
-    if ($project -and ($project -eq '.')) {
-        $packageParameters.packageId = "$($ENV:GITHUB_REPOSITORY_OWNER)-$($env:RepoName)"
-    }
-    else {
-        $packageParameters.packageId = "$($ENV:GITHUB_REPOSITORY_OWNER)-$($env:RepoName)-$projectName"
-    }
+if (-not $project -or ($project -ne '.')) {
+    $packageId += "-$projectName"
 }
 
 if ($type -eq 'CD') {
-    $packageParameters.packageId += "-preview"
+    $packageId += "-preview"
 }
 
-# Determine package version
-$packageParameters.packageVersion = $appsFolder.SubString($appsFolder.IndexOf("-Apps-") + 6)
+# Extract version from the published folders (naming convention)
+$packageVersion = $appsFolder.SubString($appsFolder.IndexOf("-Apps-") + 6)
 
-# Determine package title
-if ($nuGetAccount.ContainsKey('PackageTitle')) {
-    $packageParameters.packageTitle = $nuGetAccount.PackageTitle
-}
-else {
-     $packageParameters.packageTitle = $packageParameters.packageId
-}
-
-# Determine package description
-if ($nuGetAccount.ContainsKey('PackageDescription')) {
-    $packageParameters.packageDescription = $nuGetAccount.PackageDescription
-}
-else {
-    $packageParameters.packageDescription = $packageParameters.packageTitle
-}
-
-# Determine package authors
-if ($nuGetAccount.ContainsKey('PackageAuthors')) {
-    $packageParameters.packageAuthors = $nuGetAccount.PackageAuthors
-}
-else {
-    $packageParameters.packageAuthors = 'mazhelez'
-}
-
-Write-Host "Package parameters: "
-$packageParameters
-
-$nuspec = GenerateNuspecTemplate `
-            -PackageId $packageParameters.packageId `
-            -Version $packageParameters.packageVersion `
+$nuspec = GenerateNuspec `
+            -PackageId $packageId `
+            -Version $packageVersion `
 
 $packageFolder = Join-Path $env:TEMP ([GUID]::NewGuid().ToString())
 New-Item -Path $packageFolder -ItemType Directory | Out-Null
 
-Write-Host "Package folder: $packageFolder"
+Write-Host "Package folder: $packageFolder" -ForegroundColor Magenta
 
 try {
-    Write-Host "Copy main app files to package folder $packageFolder"
+    Write-Host "Copy main app files to package folder $packageFolder" -ForegroundColor Magenta
     (gci -Path $appsFolder -Filter "*.app") | % {
         Copy-Item -Path $_.FullName -Destination $packageFolder -Force
     
@@ -90,7 +83,7 @@ try {
         $nuspec.package.files.AppendChild($fileElement) | Out-Null
     }
     
-    Write-Host "Copy test app files to package folder $packageFolder"
+    Write-Host "Copy test app files to package folder $packageFolder" -ForegroundColor Magenta
     (gci -Path $testAppsFolder -Filter "*.app") | % {
         Copy-Item -Path $_.FullName -Destination $packageFolder -Force -Container
     
@@ -101,76 +94,44 @@ try {
         $nuspec.package.files.AppendChild($fileElement) | Out-Null
     }
     
-    Write-Host "Copy source code for project $project to package folder $packageFolder"
-    Copy-Item -Path (Join-Path $ENV:GITHUB_WORKSPACE $project) -Destination (Join-Path $packageFolder 'SourceCode') -Recurse -Force
-    $fileElement = $nuspec.CreateElement("file")
-    $fileElement.SetAttribute("src", "SourceCode\**")
-    $fileElement.SetAttribute("target", "SourceCode")
-    
-    $nuspec.package.files.AppendChild($fileElement) | Out-Null
+    Write-Host "Copy source code for project $project to package folder $packageFolder/SourceCode" -ForegroundColor Magenta
+    Copy-Item -Path (Join-Path $env:GITHUB_WORKSPACE $project) -Destination (Join-Path $packageFolder 'SourceCode') -Recurse -Force
     
     #Create .nuspec file(Join-Path $packageFolder 'manifest.nuspec')
     $nuspecFilePath = (Join-Path $packageFolder 'manifest.nuspec')
     $nuspec.Save($nuspecFilePath)
     
-    # Download nuget CLI
-    # Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile ./nuget.exe
+    $outputDirectory = Join-Path $env:GITHUB_WORKSPACE 'out'
+    New-Item -Path $outputDirectory -ItemType Directory -Force | Out-Null
+
     
-    $nugetOutput =  Invoke-Expression -Command "./nuget.exe pack $nuspecFilePath -OutputDirectory ."
+    Write-Host "Download nuget CLI" -ForegroundColor Magenta
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $outputDirectory/nuget.exe
     
-    $nugetOutput | Write-Host
+    $nugetOutput =  Invoke-Expression -Command "$outputDirectory/nuget.exe pack $nuspecFilePath -OutputDirectory $outputDirectory"
+    
+    $nugetOutput | Write-Host -ForegroundColor Magenta
     if ($LASTEXITCODE -or $null -eq $nugetOutput)
     {
         throw "Generating the nuget pack failed with exit code $LASTEXITCODE"
     }
-    
-    $nugetOutput
 }
 finally {
     Remove-Item $packageFolder -Recurse -Force
 }
 
-$nugetPackageFile = gci -Path '.' -Filter "$($packageParameters.packageId)*.nupkg"
+# Get the newly created package
+$nugetPackageFile = gci -Path $outputDirectory -Filter "$($packageParameters.packageId)*.nupkg"
 
 if(-not $nugetPackageFile) {
     throw "Cannot find nupkg file"
 }
 
-Write-Host "Push package $($nugetPackageFile.FullName) to $nuGetServerUrl"
-$nugetOutput =  Invoke-Expression -Command "./nuget.exe push $($nugetPackageFile.FullName) -ApiKey $nuGetToken -Source $nuGetServerUrl"
+Write-Host "Push package $($nugetPackageFile.FullName) to $nuGetServerUrl" -ForegroundColor Magenta
+#$nugetOutput =  Invoke-Expression -Command "./nuget.exe push $($nugetPackageFile.FullName) -ApiKey $nuGetToken -Source $nuGetServerUrl"
 
-$nugetOutput
-
-function GenerateNuspecTemplate
-(
-    [Parameter(Mandatory=$true)]
-    $PackageId,
-
-    [Parameter(Mandatory=$true)]
-    $Version
-)
+if ($LASTEXITCODE -or $null -eq $nugetOutput)
 {
-    [xml] $template = '<?xml version="1.0" encoding="utf-8"?>
-    <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
-        <metadata>
-            <id></id>
-            <version></version>
-            <title>Dynamics SMB Business Central</title>
-            <authors>Microsoft</authors>
-            <owners>Microsoft</owners>
-            <requireLicenseAcceptance>false</requireLicenseAcceptance>
-            <description>System Application for D365 Business Central</description>
-            <summary>System Application for D365 Business Central</summary>
-        </metadata>
-	    <files>
-	    </files>
-    </package>'
-
-    $template.package.metadata.id = $PackageId
-    $template.package.metadata.version = $Version
-    $template.package.files.file.Attributes['src'].Value = "$PackageSrcDir"
-    $template.package.files.file.Attributes['target'].Value = "$PackageTargetDirName"
-
-    return $template
+    throw "Pushing nuget package $($nugetPackageFile.FullName) failed with exit code $LASTEXITCODE"
 }
 
