637ffc758f7ac4d0afb36b05167a19c5c43482bb
mazhelez <mazhelez@microsoft.com>
2022-11-25 00:45:28 +0100

    Create buildArtifactsFolder if it doesn't exists

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -16,6 +16,10 @@ $appName = (gci -Path $($parameters.appProjectFolder) -Filter "app.json" | Get-C
 # Create an archive with the current source code in the build artifacts folder
 $buildArtifactsFolder = Join-Path -Path $parameters.appProjectFolder -ChildPath ".buildartifacts/Apps"
 
+if(-not (Test-Path $buildArtifactsFolder)) {
+    New-Item -Name $buildArtifactsFolder -ItemType Directory -
+}
+
 Write-Host "Archive the current source code for app: $appName in $buildArtifactsFolder"
 Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$buildArtifactsFolder/$appName.Source.zip" -Force
 
