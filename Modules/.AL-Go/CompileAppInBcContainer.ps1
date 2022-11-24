0bf9b824d9c63bf94bcd1d558bc10376d7546c40
mazhelez <mazhelez@microsoft.com>
2022-11-25 00:23:09 +0100

    😒

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,16 +2,19 @@ Param(
     [Hashtable] $parameters
 )
 
-$appName = (gci -Path $($parameters.appProjectFolder) -Filter "app.json" | Get-Content | ConvertFrom-Json).name
-
-$buildArtifactsFolder = Join-Path -Path $parameters.appProjectFolder -ChildPath ".buildartifacts/Apps" -Resolve
-
+# Setup compiler features to generate captions and LCGs
 if (!$parameters.ContainsKey("Features")) {
     $parameters["Features"] = @()
 }
 $parameters["Features"] = @("lcgtranslationfile", "generateCaptions")
 
 $appFile = Compile-AppInBcContainer @parameters
+
+# Extract app name from app.json
+$appName = (gci -Path $($parameters.appProjectFolder) -Filter "app.json" | Get-Content | ConvertFrom-Json).name
+
+# Create an archive with the current source code in the build artifacts folder
+$buildArtifactsFolder = Join-Path -Path $parameters.appProjectFolder -ChildPath ".buildartifacts/Apps"
 
 Write-Host "Archive the current source code for app: $appName in $buildArtifactsFolder"
 Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$buildArtifactsFolder/$appName.Source.zip" -Force
