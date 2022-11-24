287d5071b74c997cec880d2ed7ab0c8c390ae57f
mazhelez <mazhelez@microsoft.com>
2022-11-24 16:43:45 +0100

    Contruct better structure when building artifacts

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,6 +2,10 @@ Param(
     [Hashtable] $parameters
 )
 
+$appName = (gci -Path $($parameters.appProjectFolder) -Filter "app.json" | Get-Content | ConvertFrom-Json).name
+
+$parameters.appOutputFolder = Join-Path -Path $parameters.appOutputFolder -ChildPath $appName
+
 if (!$parameters.ContainsKey("Features")) {
     $parameters["Features"] = @()
 }
@@ -9,7 +13,7 @@ $parameters["Features"] = @("lcgtranslationfile", "generateCaptions")
 
 $appFile = Compile-AppInBcContainer @parameters
 
-Write-Host "Archive the current source code for app: $($parameters.appName) in $($parameters.appOutputFolder)"
-Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$($parameters.appOutputFolder)/$($parameters.appName).zip" -Force
+Write-Host "Archive the current source code for app: $appName in $($parameters.appOutputFolder)"
+Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$($parameters.appOutputFolder)/$appName.Source.zip" -Force
 
 $appFile
