5d379bc2e9a28ae1bd79858fced81131ad66b730
mazhelez <mazhelez@microsoft.com>
2022-11-24 14:29:17 +0100

    Compress source code when building the app

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -9,33 +9,7 @@ $parameters["Features"] = @("lcgtranslationfile", "generateCaptions")
 
 $appFile = Compile-AppInBcContainer @parameters
 
-if ($appFile) {
-    $filename = [System.IO.Path]::GetFileName($appFile)
-    if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
-        # System application compiled - add BaseApp and Application app from container to output
-        Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { Param([string]$packagesFolder)
-            function CopyApp {
-                Param(
-                    [string] $localAppPath,
-                    [string] $w1AppPath
-                )
-
-                $name = [System.IO.Path]::GetFileName($w1AppPath)
-                Write-Host "Copying $name to packages path"
-                if (Test-Path $localAppPath) {
-                    $appPath = $localAppPath
-                }
-                else {
-                    $appPath = $w1AppPath
-                }
-                Copy-Item -Path $appPath -Destination (Join-Path $packagesFolder $name)
-                Copy-Item -Path $appPath -Destination (Join-Path "c:\run\my" $name)
-            }
-
-            CopyApp -localAppPath "C:\Applications.*\Microsoft_Base Application_*.*.*.*.app"    -w1AppPath "C:\Applications\BaseApp\Source\Microsoft_Base Application.app"
-            CopyApp -localAppPath "C:\Applications.*\Microsoft_Application_*.*.*.*.app"         -w1AppPath "C:\Applications\Application\Source\Microsoft_Application.app" 
-        } -argumentList (Get-BcContainerPath -ContainerName $parameters.ContainerName -path $Parameters.appSymbolsFolder)
-    }
-}
+Write-Host "Archive the current source code for app: $($parameters.appName) in $($parameters.appOutputFolder)"
+Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$($parameters.appOutputFolder)/$($parameters.appName).zip" -Force
 
 $appFile
