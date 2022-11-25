1fb9d76094798639eb1d33f7d3870c58a0836324
mazhelez <mazhelez@microsoft.com>
2022-11-25 08:16:56 +0100

    Desperate times... 😒

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -13,14 +13,17 @@ $appFile = Compile-AppInBcContainer @parameters
 # Extract app name from app.json
 $appName = (gci -Path $($parameters.appProjectFolder) -Filter "app.json" | Get-Content | ConvertFrom-Json).name
 
+Write-Host "Current app name: $appName"
+
 # Create an archive with the current source code in the build artifacts folder
-$buildArtifactsFolder = Join-Path -Path $parameters.appProjectFolder -ChildPath ".buildartifacts/Apps"
+$buildArtifactsFolder = "$($parameters.appProjectFolder)/.buildartifacts/Apps"
 
 if(-not (Test-Path $buildArtifactsFolder)) {
+    Write-Host "Creating $buildArtifactsFolder"
     New-Item -Name $buildArtifactsFolder -ItemType Directory
 }
 
-Write-Host "Archive the current source code for app: $appName in $buildArtifactsFolder"
+Write-Host "Archive the current source code for app: $appName in $buildArtifactsFolder as $($appName).Source.zip"
 Compress-Archive -Path "$($parameters.appProjectFolder)" -DestinationPath "$buildArtifactsFolder/$appName.Source.zip" -Force
 
 $appFile
