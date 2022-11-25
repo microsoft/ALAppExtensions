e010895454fc1ddc2ce5398c17b2c1a703480701
mazhelez <mazhelez@microsoft.com>
2022-11-25 11:40:30 +0100

    Hardcode buildArtifactsFolder

--- Modules/.AL-Go/CompileAppInBcContainer.ps1
+++ Modules/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,18 +2,6 @@ Param(
     [Hashtable] $parameters
 )
 
-function FindALGoProject($appProjectFolder)
-{
-    $currentFolder = Get-Item $appProjectFolder
-
-    do {
-        $currentFolder = $currentFolder.Parent
-    }
-    while(-not (Test-Path "$currentFolder/.AL-Go"))
-
-    return $currentFolder.FullName
-}
-
 # Setup compiler features to generate captions and LCGs
 if (!$parameters.ContainsKey("Features")) {
     $parameters["Features"] = @()
@@ -35,22 +23,11 @@ $archiveFile = "$env:TEMP/$appName.Source.zip"
 Write-Host "Archive the current source code for app: $appName as $archiveFile"
 Compress-Archive -Path "$appProjectFolder" -DestinationPath $archiveFile -Force
 
+$buildArtifactsFolder = "$env:GITHUB_WORKSPACE/Modules/.buildartifacts/Apps" # hackidy-hack
 
-if ($parameters['buildArtifactFolder']) {
-    Write-Host "Found artifacts folder in parameters!"
-    $buildArtifactsFolder = $parameters['buildArtifactFolder']
-}
-else {
-    $ALGoProjectFolder = FindALGoProject -appProjectFolder $appProjectFolder
-    Write-Host "AL-Go project: $ALGoProjectFolder"
-    $buildArtifactsFolder = ".buildartifacts/Apps" # hackidy-hack
-    
-    if(-not (Test-Path "$ALGoProjectFolder/$buildArtifactsFolder")) {
-        Write-Host "Creating $buildArtifactsFolder in $ALGoProjectFolder"
-        New-Item -Path "$ALGoProjectFolder" -Name $buildArtifactsFolder -ItemType Directory
-    }
-
-    $buildArtifactsFolder = "$ALGoProjectFolder/$buildArtifactsFolder"
+if(-not (Test-Path $buildArtifactsFolder)) {
+    Write-Host "Creating $buildArtifactsFolder"
+    New-Item -Path "$env:GITHUB_WORKSPACE/Modules" -Name ".buildartifacts/Apps" -ItemType Directory
 }
 
 Write-Host "Build artifacts folder: $buildArtifactsFolder"
