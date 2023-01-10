5efb18325c93c68e7d25ee24d6252247b6e282a5
Alexander Holstrup <aholstrup@microsoft.com>
2023-01-10 14:00:59 +0100

    refactor away from Deliver

--- .github/DeliverToBCFeed.ps1
+++ .github/DeliverToBCFeed.ps1
@@ -1,12 +1,4 @@
-Param(
-    [Hashtable] $parameters
-)
-<#
-$project = "$parameters.project"
-$projectName = "Modules"
-$appsFolders = $parameters.appsFolders
-$testAppsFolders = $parameters.testAppsFolders
-#>
+
 function GenerateManifest
 (
     [Parameter(Mandatory=$true)]
@@ -26,32 +18,22 @@ function GenerateManifest
     $template.package.metadata.authors = $Authors
     $template.package.metadata.owners = $Owners
 
-    return $template
+    $template.Save("$PSScriptRoot\ALAppExtensions.template.nuspec")
 }
 
-Write-Host $parameters -ForegroundColor Magenta
+$buildArtifactsPath = Join-Path $env:GITHUB_WORKSPACE '.artifacts'
+$packageFolder = Join-Path $env:GITHUB_WORKSPACE 'out'
 
-$project = $parameters.project
-$projectName = $parameters.projectName
-$appsFolders = $parameters.appsFolders
-$testAppsFolders = $parameters.testAppsFolders
+$projectName = "Modules" 
+$appsFolders = Get-ChildItem $buildArtifactsPath -Directory | where-object {$_.FullName.Contains("Apps-")} | Select-Object -ExpandProperty FullName
+$testAppsFolders = Get-ChildItem $buildArtifactsPath -Directory | where-object {$_.FullName.Contains("TestApps-")} | Select-Object -ExpandProperty FullName
+$packageVersion = ($appsFolders -replace ".*-Apps-","" | Select-Object -First 1).ToString() #version is right after '-Apps-'
 
 Write-Host "App folder(s): $($appsFolders -join ', ')" -ForegroundColor Magenta
 Write-Host "Test app folder(s): $($testAppsFolders -join ', ')" -ForegroundColor Magenta
 
-$artifactsPath = Join-Path $env:GITHUB_WORKSPACE '.artifacts'
 
-if (test-path $artifactsPath) {
-    $testAppsFolders2 = Get-ChildItem $artifactsPath -Directory | where-object {$_.FullName.Contains("TestApps-")} | Select-Object -ExpandProperty FullName
-    $AppsFolders2 = Get-ChildItem $artifactsPath -Directory | where-object {$_.FullName.Contains("Apps-")} | Select-Object -ExpandProperty FullName
-
-    Write-Host "App folder(s) 2: $($AppsFolders2 -join ', ')" -ForegroundColor Magenta
-    Write-Host "Test app folder(s) 2: $($testAppsFolders2 -join ', ')" -ForegroundColor Magenta
-
-} else {
-    Write-Host "No artifacts folder found in path $artifactsPath" -ForegroundColor Magenta
-}
-
+### Generate Nuspec file
 # Construct package ID
 if ($ENV:GITHUB_REF_NAME -eq "main") {
     $packageId = "$($env:GITHUB_REPOSITORY_OWNER)-$($env:RepoName)-$projectName-preview"
@@ -60,25 +42,17 @@ if ($ENV:GITHUB_REF_NAME -eq "main") {
 }
 
 Write-Host "Package ID: $packageId" -ForegroundColor Magenta
-
-# Extract version from the published folders (naming convention)
-$packageVersion = ($appsFolders -replace ".*-Apps-","" | Select-Object -First 1).ToString() #version is right after '-Apps-'
-
 Write-Host "Package version: $packageVersion" -ForegroundColor Magenta
 
-$manifest = GenerateManifest `
-            -PackageId $packageId `
-            -Version $packageVersion `
-            -Authors "$env:GITHUB_REPOSITORY_OWNER" `
-            -Owners "$env:GITHUB_REPOSITORY_OWNER"
+GenerateManifest `
+    -PackageId $packageId `
+    -Version $packageVersion `
+    -Authors "$env:GITHUB_REPOSITORY_OWNER" `
+    -Owners "$env:GITHUB_REPOSITORY_OWNER"
 
-# Create a temp folder to use for the packaging
-$packageFolder = Join-Path $env:GITHUB_WORKSPACE 'out'
-#$packageFolder = Join-Path $env:TEMP ([GUID]::NewGuid().ToString())
+### Copy files to package folder
 New-Item -Path $packageFolder -ItemType Directory | Out-Null
-
 Write-Host "Package folder: $packageFolder" -ForegroundColor Magenta
-
 
 # Create folder to hold the apps
 New-Item -Path "$packageFolder/Apps" -ItemType Directory -Force | Out-Null
@@ -94,6 +68,3 @@ New-Item -Path "$packageFolder/Apps" -ItemType Directory -Force | Out-Null
 
 # Copy over the license file
 Copy-Item -Path "$env:GITHUB_WORKSPACE/LICENSE" -Destination "$packageFolder/Apps" -Force
-
-#Create .nuspec file
-$manifest.Save("$PSScriptRoot\ALAppExtensions.template.nuspec")
