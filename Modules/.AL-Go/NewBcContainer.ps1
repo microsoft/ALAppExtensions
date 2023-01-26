1328a8caa53c1ed9b491f812fc05167ebb4fb3bd
mazhelez <mazhelez@microsoft.com>
2023-01-26 15:59:53 +0100

    Do not install System App when creating a container

--- Modules/.AL-Go/NewBcContainer.ps1
+++ Modules/.AL-Go/NewBcContainer.ps1
@@ -16,10 +16,11 @@ $parameters.auth = 'Windows'
 New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
-$installedApps | ForEach-Object {
-    $removeData = $_.Name -ne "Base Application"
-    Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
-}
+$installedApps | Where-Object { $_.Name -ne 'System Application' } | ForEach-Object {
+        $removeData = $_.Name -ne "Base Application"
 
+        Write-Host "Removing $($_.Name)"
+        Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
+    }
+    
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
