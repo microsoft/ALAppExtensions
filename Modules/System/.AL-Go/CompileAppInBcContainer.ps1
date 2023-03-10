99ba017f0597e17d8b7bcdd73bba46755b746527
mazhelez <mazhelez@microsoft.com>
2023-03-10 17:04:19 +0100

    Build System Application from an outside AL-Go project

--- Modules/System/.AL-Go/CompileAppInBcContainer.ps1
+++
@@ -1,5 +0,0 @@
-Param(
-    [Hashtable] $parameters
-)
-
-. "$env:GITHUB_WORKSPACE/Build/Scripts/CompileAppInBcContainer.ps1" -parameters $parameters -currentProjectFolder (Join-Path $env:GITHUB_WORKSPACE "Modules/System")
