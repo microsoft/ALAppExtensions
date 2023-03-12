aca0d357d7eceb739533f34e594895964c7fbb8f
mazhelez <mazhelez@microsoft.com>
2023-03-13 00:59:47 +0100

    Revert "[To be reverted] Publish artifacts from current branch"
    This reverts commit 3467679d320936ba16137f77606928e0998a61e3.

--- Build/Scripts/CompileAppInBcContainer.ps1
+++ Build/Scripts/CompileAppInBcContainer.ps1
@@ -26,8 +26,8 @@ $appFile = Compile-AppInBcContainer @parameters
 
 $branchName = $ENV:GITHUB_REF_NAME
 
-# Only add the source code and other artifacts to the build artifacts if the delivering is allowed on the branch 
-if($branchName -and (($branchName -eq 'build-app-modules') -or $branchName.StartsWith('release/'))) {
+# Only add the source code to the build artifacts if the delivering is allowed on the branch 
+if($branchName -and (($branchName -eq 'main') -or $branchName.StartsWith('release/'))) {
     $appProjectFolder = $parameters.appProjectFolder
     
     # Extract app name from app.json
