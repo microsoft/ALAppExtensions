e5e34e9f8689c1e458802d37cb904f8ce1a8bf93
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2023-03-16 16:39:18 +0100

    Distinguish CICD build from PR build (#22568)

--- Build/Scripts/CompileAppInBcContainer.ps1
+++ Build/Scripts/CompileAppInBcContainer.ps1
@@ -7,11 +7,9 @@ Param(
 Import-Module $PSScriptRoot\GuardingV2ExtensionsHelper.psm1
 Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
 
-$branchName = Get-GitBranchName
-$RepoRootFolder = Get-BaseFolder
 $appBuildMode = Get-BuildMode
 
-# $app is a variable that determine whether the current app is a normal app (not test app, for instance)
+# $app is a variable that determines whether the current app is a normal app (not test app, for instance)
 if($app)
 {
     # Setup compiler features to generate captions and LCGs
@@ -29,67 +27,17 @@ if($app)
     if (($parameters.ContainsKey("EnableAppSourceCop") -and $parameters["EnableAppSourceCop"]) -or ($parameters.ContainsKey("EnablePerTenantExtensionCop") -and $parameters["EnablePerTenantExtensionCop"])) {
         Enable-BreakingChangesCheck -AppSymbolsFolder $parameters["appSymbolsFolder"] -AppProjectFolder $parameters["appProjectFolder"] -BuildMode $appBuildMode | Out-Null
     }
-    
 }
 
 $appFile = Compile-AppInBcContainer @parameters
 
-# Only add the source code to the build artifacts if the delivering is allowed on the branch 
-if($branchName -and (($branchName -eq 'main') -or $branchName.StartsWith('release/'))) {
-    $appProjectFolder = $parameters["appProjectFolder"]
-    
-    # Extract app name from app.json
-    $appName = (Get-ChildItem -Path $appProjectFolder -Filter "app.json" | Get-Content | ConvertFrom-Json).name
+# Determine whether the current build is a CICD build
+$CICDBuild = $env:GITHUB_WORKFLOW -and ($($env:GITHUB_WORKFLOW).Trim() -eq 'CI/CD')
 
-    Write-Host "Current app name: $appName; app folder: $appProjectFolder"
-
-    # Determine the folder where the artifacts for the package will be stored
-    $holderFolder = 'Apps'
-    if(-not $app) {
-        $holderFolder = 'TestApps'
-    }
-
-    $packageArtifactsFolder = "$currentProjectFolder/.buildartifacts/$holderFolder/Package/$appName/$appBuildMode" # manually construct the artifacts folder
-    
-    
-    if(-not (Test-Path $packageArtifactsFolder)) {
-        Write-Host "Creating $packageArtifactsFolder"
-        New-Item -Path "$currentProjectFolder" -Name ".buildartifacts/$holderFolder/Package/$appName/$appBuildMode" -ItemType Directory | Out-Null
-    }
-    
-    Write-Host "Package artifacts folder: $packageArtifactsFolder"
-    
-    $buildArtifactsFolder = Join-Path "$packageArtifactsFolder" "BuildArtifacts"
-    $sourceCodeFolder = Join-Path "$packageArtifactsFolder" "SourceCode"
-
-    switch ( $appBuildMode )
-    {
-        'Default' { 
-            # Add the generated Translations folder to the artifacts folder
-            $TranslationsFolder = Join-Path "$appProjectFolder" "Translations"
-            if (Test-Path $TranslationsFolder) {
-                Write-Host "Copying translation for app $appName from $TranslationsFolder to $buildArtifactsFolder"
-                Copy-Item -Path $TranslationsFolder -Destination "$buildArtifactsFolder" -Recurse -Force | Out-Null
-            } else {
-                Write-Host "Translations were not generated for app $appName"
-            }
-
-            # Add the source code to the artifacts folder
-            Write-Host "Copying source code for app '$appName' from '$appProjectFolder' to source code folder: $sourceCodeFolder"
-            Copy-Item -Path "$appProjectFolder" -Destination "$sourceCodeFolder" -Recurse -Force | Out-Null
-         }
-        'Translated' { 
-            # Add the source code for non-test apps to the artifacts folder as it contains the translations
-            if($app) {
-                Write-Host "Copying source code for app '$appName' from '$appProjectFolder' to source code folder: $sourceCodeFolder"
-                Copy-Item -Path "$appProjectFolder" -Destination "$sourceCodeFolder" -Recurse -Force | Out-Null
-            }
-        }
-    }
-    
-    # Add the app file for every built app to a folder for all built modes
-    Write-Host "Copying app file for app '$appName' from '$appFile' to build artifacts folder: $packageArtifactsFolder"
-    Copy-Item -Path $appFile -Destination $packageArtifactsFolder -Force | Out-Null
+if($CICDBuild) {
+    # Create the artifacts folder for the app to place in the package
+    . $PSScriptRoot\CreateAppPackageOutput.ps1 -AppProjectFolder $parameters["appProjectFolder"] -BuildMode $appBuildMode -AppFile $appFile -ALGoProjectFolder $currentProjectFolder -IsTestApp:$(!$app)
 }
 
+# Return the app file path 
 $appFile
