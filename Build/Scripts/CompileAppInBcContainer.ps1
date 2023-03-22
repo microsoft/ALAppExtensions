4bd86e3bb9c7f60c17dec4fd098f43299d363cad
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-22 16:18:28 +0100

    Only run packaging if needed

--- Build/Scripts/CompileAppInBcContainer.ps1
+++ Build/Scripts/CompileAppInBcContainer.ps1
@@ -6,6 +6,7 @@ Param(
 
 Import-Module $PSScriptRoot\GuardingV2ExtensionsHelper.psm1
 Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
+Import-Module $PSScriptRoot\Package\PackNuget.psm1
 
 $appBuildMode = Get-BuildMode
 
@@ -31,10 +32,12 @@ if($app)
 
 $appFile = Compile-AppInBcContainer @parameters
 
+Write-Host "App Name: $($parameters['appName'])"
 # Determine whether the current build is a CICD build
 $CICDBuild = $env:GITHUB_WORKFLOW -and ($($env:GITHUB_WORKFLOW).Trim() -eq 'CI/CD')
+$includeAppInPackage = Add-AppToPackage -ApplicationName $parameters["appName"]
 
-if($CICDBuild) {
+if($CICDBuild -and $includeAppInPackage) {
     # Create the artifacts folder for the app to place in the package
     . $PSScriptRoot\Package\CreateAppPackageOutput.ps1 -AppProjectFolder $parameters["appProjectFolder"] -BuildMode $appBuildMode -AppFile $appFile -ALGoProjectFolder $currentProjectFolder -IsTestApp:$(!$app)
 }
