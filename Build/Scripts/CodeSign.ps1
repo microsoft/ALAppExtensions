8c336e4ff1298f9d5d5148bac07b7731aa899009
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-28 16:00:07 +0200

    Logging

--- Build/Scripts/CodeSign.ps1
+++ Build/Scripts/CodeSign.ps1
@@ -16,13 +16,17 @@ param(
 
 #$ClientSecret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AzureKeyVaultClientSecret))
 
+Write-Host "Signing files: $Files"
+Write-Host "AzureKeyVaultCertificateName: $AzureKeyVaultCertificateName"
+
+
 AzureSignTool.exe sign $Files `
                         --azure-key-vault-url "$AzureKeyVaultURI" `
-                        --azure-key-vault-client-id "$AzureKeyVaultClientID"`
-                        --azure-key-vault-client-secret "$ClientSecret"`
-                        --azure-key-vault-tenant-id "$AzureKeyVaultTenantID"`
+                        --azure-key-vault-client-id "$AzureKeyVaultClientID" `
+                        --azure-key-vault-client-secret "$ClientSecret" `
+                        --azure-key-vault-tenant-id "$AzureKeyVaultTenantID" `
                         --azure-key-vault-certificate "$AzureKeyVaultCertificateName" `
-                        --description "$Description"`
+                        --description "$Description" `
                         --description-url "$DescriptionUrl" `
                         --timestamp-rfc3161 "$TimestampService" `
                         --timestamp-digest "$TimestampDigest" `
