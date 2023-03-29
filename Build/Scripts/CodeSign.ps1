ab0cf0a31a8d25c3767f1dbee9d9a1f37775fe16
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-29 11:22:19 +0200

    Update codesign

--- Build/Scripts/CodeSign.ps1
+++ Build/Scripts/CodeSign.ps1
@@ -10,6 +10,20 @@ param(
     [string]$FileDigest = "sha256"
 )
 
+# TODO: Remove when moving to AL-GO
+$webClient = New-Object System.Net.WebClient
+$webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
+$webClient.Encoding = [System.Text.Encoding]::UTF8
+Write-Host "Downloading GitHub Helper module"
+$GitHubHelperPath = "$([System.IO.Path]::GetTempFileName()).psm1"
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/preview/Github-Helper.psm1', $GitHubHelperPath)
+Write-Host "Downloading AL-Go Helper script"
+$ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/preview/AL-Go-Helper.ps1', $ALGoHelperPath)
+
+Import-Module $GitHubHelperPath
+. $ALGoHelperPath -local
+
 $BcContainerHelperPath = DownloadAndImportBcContainerHelper -baseFolder $ENV:GITHUB_WORKSPACE 
 
 Install-NAVSipCryptoProviderFromNavContainer 
