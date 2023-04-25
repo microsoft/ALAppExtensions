f2e97bec2589e07bab086ab070deca2173296dbe
Alexander Holstrup <aholstrup@microsoft.com>
2023-04-25 14:48:21 +0200

    fix

--- System Application/.AL-Go/CompileAppInBcContainer.ps1
+++ System Application/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,5 +2,6 @@ Param(
     [Hashtable] $parameters
 )
 
-$script = Join-Path $PSScriptRoot "../../Build/Scripts/CompileAppInBcContainer.ps1" -Resolve
-. $script -parameters $parameters -currentProjectFolder (Join-Path $env:GITHUB_WORKSPACE "System Application")
+$scriptPath = Join-Path $PSScriptRoot "../../Build/Scripts/CompileAppInBcContainer.ps1" -Resolve
+$projectFolder = Join-Path $PSScriptRoot "../../System Application"
+. $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
