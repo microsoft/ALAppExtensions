8ea2f334885b96bd18a93c0ab3778d8e393a7a67
Alexander Holstrup <aholstrup@microsoft.com>
2023-04-25 16:07:30 +0200

    build stability tools

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
-Add-Type -AssemblyName System.Web
-$credential = New-Object pscredential admin, (ConvertTo-SecureString -String ([System.Web.Security.Membership]::GeneratePassword(10, 3)) -AsPlainText -Force)
+param
+(
+    [string] $ALGoProject = 'Test Stability Tools'
+)
 
-$scriptPath = Join-Path $PSScriptRoot "System Application\.AL-Go\localDevEnv.ps1" -Resolve
-& $scriptPath -containerName test -auth UserPassword -credential $credential -licenseFileUrl "none"
+Add-Type -AssemblyName System.Web
+$credential = New-Object pscredential admin, (ConvertTo-SecureString -String ([System.Web.Security.Membership]::GeneratePassword(20, 5)) -AsPlainText -Force)
+
+$scriptPath = Join-Path $PSScriptRoot "$ALGoProject\.AL-Go\localDevEnv.ps1" -Resolve
+& $scriptPath -containerName test -auth UserPassword -credential $credential -licenseFileUrl 'none'
