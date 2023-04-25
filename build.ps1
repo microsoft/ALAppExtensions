39d10c7e4666393d4555c7a675692a751ea27bfa
Alexander Holstrup <aholstrup@microsoft.com>
2023-04-25 13:16:09 +0200

    Update to build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Web
 $credential = New-Object pscredential admin, (ConvertTo-SecureString -String ([System.Web.Security.Membership]::GeneratePassword(10, 3)) -AsPlainText -Force)
 
-Invoke-Expression "$PSScriptRoot\System Application\.AL-Go\localDevEnv.ps1 -containerName test -auth UserPassword -credential $credential"
+$scriptPath = Join-Path $PSScriptRoot "System Application\.AL-Go\localDevEnv.ps1" -Resolve
+& $scriptPath -containerName test -auth UserPassword -credential $credential -licenseFileUrl "none"
