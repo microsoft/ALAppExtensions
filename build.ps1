04f807ec32e4fc9852649af3851219d816ac8e16
Alexander Holstrup <aholstrup@microsoft.com>
2023-04-27 13:10:01 +0200

    Use sas token

--- build.ps1
+++ build.ps1
@@ -1,13 +1,14 @@
 param
 (
     [Parameter(Mandatory=$true)]
-    [string] $ALGoProject
+    [string] $ALGoProject,
+    [string] $InsiderSasToken = ""
 )
 
 Add-Type -AssemblyName System.Web
 $credential = New-Object pscredential admin, (ConvertTo-SecureString -String ([System.Web.Security.Membership]::GeneratePassword(20, 5)) -AsPlainText -Force)
 
 $scriptPath = Join-Path $PSScriptRoot "$ALGoProject\.AL-Go\localDevEnv.ps1" -Resolve
-& $scriptPath -containerName test -auth UserPassword -credential $credential -licenseFileUrl 'none'
+& $scriptPath -containerName test -auth UserPassword -credential $credential -licenseFileUrl 'none' -insiderSasToken $InsiderSasToken
 
 Write-Host "Last exit code: $LASTEXITCODE"
