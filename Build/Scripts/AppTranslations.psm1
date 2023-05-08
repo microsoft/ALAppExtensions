1429e6068f9ce2fea81c474d6cdd42d08cfb4101
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2023-05-08 11:13:43 +0200

    Restores translations for apps during compilation (#23189)
    - Add Packages.json to describe the external packages that are needed
    during build (for now translations and baselines)
    - Add function to intstall a package
     - Clean up unused functions
    
    - [x] Fix the automation that updates the baselines package version
    - [x] Add automation to update the translations package version
    - [x] Wait untill there is a translation package for the current version

---
+++ Build/Scripts/AppTranslations.psm1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+    Restores translations from an app.
+.Description
+    This function restores translations for an app.
+    All available translations in the translation package will be restored in a 'Translations' folder in the app project folder.
+.Parameter AppProjectFolder
+    The path to the app project folder
+#>
+function Restore-TranslationsForApp {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $AppProjectFolder
+    )
+    # Translations need to be restored in the Translations folder in the app folder
+    $appTranslationsFolder = Join-Path $AppProjectFolder "Translations"
+    $appName = (Get-ChildItem -Path $AppProjectFolder -Filter "app.json" | Get-Content | ConvertFrom-Json).name
+   
+    Write-Host "Restoring translations for app $appName in $appTranslationsFolder"
+
+    Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1 -DisableNameChecking
+    $translationsOutputFolder = Join-Path (Get-BaseFolder) "out/translations/"
+    $translationPackagePath = Install-PackageFromConfig -PackageName "AppTranslations" -OutputPath $translationsOutputFolder
+    $tranlationsPath = Join-Path $translationPackagePath "Translations"
+
+    $translationsFound = $false
+    
+    # Copy the translations from the package to the app folder
+    Get-ChildItem $tranlationsPath -Filter *-* -Directory | ForEach-Object {
+        $localeFolder = $_.FullName
+        $locale = $_.Name
+        
+        # Translations are located in the ExtensionsV2 folder
+        $translationFilePath = Join-Path $localeFolder "ExtensionsV2/$appName.$locale.xlf" 
+        if(Test-Path $translationFilePath) {
+            Write-Host "Using translation for $appName in $locale"
+            $translationsFound = $true
+
+            Copy-Item -Path $translationFilePath -Destination $appTranslationsFolder -Force | Out-Null
+        }
+    }
+
+    if (-not $translationsFound) {
+        Write-Host "No translations found for $appName"
+    }
+}
+
+Export-ModuleMember -Function *-*
