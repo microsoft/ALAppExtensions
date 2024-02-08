b060b8c02a1c37c2f85fdf6a9ba972665ecb8d71
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2024-02-08 13:07:30 +0100

    Update NewBcContainer.ps1

--- Build/Scripts/NewBcContainer.ps1
+++ Build/Scripts/NewBcContainer.ps1
@@ -15,7 +15,7 @@ New-BcContainer @parameters
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
     Write-Host "Removing $($_.Name)"
-    if ($_.Name -notin @("System Application", "Base Application", "Application")) {
+    if ($_.Name -notin @("System Application", "Business Foundation", "Base Application", "Application")) {
         Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
     } else {
         Write-Host "Skipping $($_.Name)"
