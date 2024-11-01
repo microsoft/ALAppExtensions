3b801e7317e22faf42a71d5e5dfc212f9584b8de
aholstrup1 <aholstrup@microsoft.com>
2024-11-01 14:30:28 +0100

    Try

---
+++ Build/Scripts/ImportTestDataInBcContainer.ps1
@@ -0,0 +1,43 @@
+Param(
+    [Parameter(ParameterSetName="ALGo")]
+    [Hashtable]$parameters,
+    [Parameter(ParameterSetName="Manual")]
+    [string]$containerName
+)
+
+function Get-NavDefaultCompanyName
+{
+    return "CRONUS International Ltd."
+}
+
+if ($PSCmdlet.ParameterSetName -eq 'ALGo') {
+    $containerName = $parameters.ContainerName
+}
+
+# Get the repoversion
+Import-Module "$PSScriptRoot\EnlistmentHelperFunctions.psm1"
+
+$installedApps = Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast
+$appsToBeUnPublished = (Get-ConfigValue -ConfigType "BuildConfig" -Key "AppsNotToBePublished")
+$installedApps | ForEach-Object {
+    if ($_.Name -in $appsToBeUnPublished) {
+        Write-Host "Unpublishing $($_.Name)"
+        Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
+    }
+}
+
+try {
+    $repoVersion = Get-ConfigValue -ConfigType "AL-GO" -Key "RepoVersion"
+    $DemoDataType = "EXTENDED"
+
+    Write-Host "Initializing company"
+    Invoke-NavContainerCodeunit -Codeunitid 2 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName)
+
+    Write-Host "Importing configuration package"
+    Invoke-NavContainerCodeunit -Codeunitid 8620 -containerName $containerName -CompanyName (Get-NavDefaultCompanyName) -MethodName "ImportAndApplyRapidStartPackage" -Argument "C:\ConfigurationPackages\NAV$($repoVersion).W1.ENU.$($DemoDataType).rapidstart"
+} catch {
+    Write-Host "Error while importing configuration package"
+    Write-Host $_.Exception.Message
+    Write-Host $_.Exception.StackTrace
+    exit 1
+}
