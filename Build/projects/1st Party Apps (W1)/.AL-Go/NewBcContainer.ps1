9958915d7c086ac8c33cef57638fbb8ce7285ad1
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-01-08 12:03:10 +0100

    Update NewBcContainer.ps1

--- Build/projects/1st Party Apps (W1)/.AL-Go/NewBcContainer.ps1
+++ Build/projects/1st Party Apps (W1)/.AL-Go/NewBcContainer.ps1
@@ -1,8 +1,5 @@
 Param(
     [Hashtable]$parameters
 )
-
-if ("$env:GITHUB_RUN_ID" -eq "") {
-    $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-    . $script -parameters $parameters
-}
+$script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
+. $script -parameters $parameters
