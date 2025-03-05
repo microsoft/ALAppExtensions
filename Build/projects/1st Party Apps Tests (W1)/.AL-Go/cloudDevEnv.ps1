c46b38785dc23669fd9433eb8f343d6513cfe192
bcbuild-github-agent <137281497+bcbuild-github-agent@users.noreply.github.com>
2025-03-05 14:11:49 +0100

    [main] Update AL-Go System Files from microsoft/AL-Go-PTE@preview -  b608fc6787759aaac18f1407750c3436b6553b88 (#28045)
    ## v6.3
    
    ### Deprecations
    
    - `cleanModePreprocessorSymbols` will be removed after April 1st 2025.
    Use [Conditional
    Settings](https://aka.ms/algosettings#conditional-settings) instead,
    specifying buildModes and the `preprocessorSymbols` setting. Read
    [this](https://aka.ms/algodeprecations#cleanModePreprocessorSymbols) for
    more information.
    
    ### Issues
    
    - It is now possible to skip the modification of dependency version
    numbers when running the Increment Version number workflow or the Create
    Release workflow
    
    ### New Repository Settings
    
    -
    [`shortLivedArtifactsRetentionDays`](https://aka.ms/algosettings#shortLivedArtifactsRetentionDays)
    determines the number of days to keep short lived build artifacts (f.ex
    build artifacts from pull request builds, next minor or next major
    builds). 1 is default. 0 means use GitHub default.
    -
    [`preProcessorSymbols`](https://aka.ms/algosettings#preProcessorSymbols)
    is a list of preprocessor symbols to use when building the apps. This
    setting can be specified in [workflow specific settings
    files](https://aka.ms/algosettings#where-are-the-settings-located) or in
    [conditional
    settings](https://aka.ms/algosettings#conditional-settings).
    
    ### New Versioning Strategy
    
    Setting versioning strategy to 3 will allow 3 segments of the version
    number to be defined in app.json and repoVersion. Only the 4th segment
    (Revision) will be defined by the GitHub
    [run_number](https://go.microsoft.com/fwlink/?linkid=2217416&clcid=0x409)
    for the CI/CD workflow. Increment version number and Create Release now
    also supports the ability to set a third segment to the RepoVersion and
    appversion in app.json.
    
    ### Change in published artifacts
    
    When using `useProjectDependencies` in a multi-project repository, AL-Go
    for GitHub used to generate short lived build artifacts called
    `thisBuild-<projectnaame>-<type>-...`. This is no longer the case.
    Instead, normal build artifacts will be published and used by depending
    projects. The retention period for the short lived artifacts generated
    are controlled by a settings called
    [`shortLivedArtifactsRetentionDays`](https://aka.ms/algosettings#shortLivedArtifactsRetentionDays).
    
    ### Preprocessor symbols
    
    It is now possible to define preprocessor symbols, which will be used
    when building your apps using the
    [`preProcessorSymbols`](https://aka.ms/algosettings#preProcessorSymbols)
    setting. This setting can be specified in workflow specific settings
    file or it can be used in conditional settings.
    
    
    [AB#539394](https://dynamicssmb2.visualstudio.com/1fcb79e7-ab07-432a-a3c6-6cf5a88ba4a5/_workitems/edit/539394)
    
    Co-authored-by: aholstrup1 <aholstrup1@users.noreply.github.com>

--- Build/projects/1st Party Apps Tests (W1)/.AL-Go/cloudDevEnv.ps1
+++ Build/projects/1st Party Apps Tests (W1)/.AL-Go/cloudDevEnv.ps1
@@ -42,9 +42,9 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.2/Github-Helper.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.2/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.2/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.3/Github-Helper.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.3/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.3/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
